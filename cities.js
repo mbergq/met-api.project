914f37c5d6aec65115c2915e4b42fd6db31260fa
Martin <106612709+menkodot@users.noreply.github.com>
2023-11-07 09:51:59 +0100

    create divs via js instead, form submit not working atm

--- cities.js
+++ cities.js
@@ -5,9 +5,17 @@ addEventListener('DOMContentLoaded', () => {
       console.log(data.length);
       console.log(document.querySelectorAll('.dataContainer')[3]);
 
+      //Create elements with data from API
       for (let i = 0; i < data.length; i++) {
+        //Create containers for data info
+        let div = document.createElement('div');
+        div.classList.add('dataContainer');
+        document.getElementById('container').appendChild(div);
+
+        //Append data info to containers
         let name = document.createElement('p');
         let population = document.createElement('p');
+
         name.textContent = data[i].name;
         population.textContent = data[i].population;
 
@@ -17,4 +25,23 @@ addEventListener('DOMContentLoaded', () => {
       }
 
     })
+})
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const info = new FormData(form);
+  console.log(info);
+
+  fetch('https://avancera.app/cities/', {
+    body: info,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
 })
