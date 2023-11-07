628ff2bb558c8fb1ccc3b8e18389a2dd7172ca4d
Martin <106612709+menkodot@users.noreply.github.com>
2023-11-07 15:01:25 +0100

    added PUTform function

--- cities.js
+++ cities.js
@@ -7,41 +7,43 @@ addEventListener('DOMContentLoaded', () => {
       //Create elements with data from API
       for (let i = 0; i < data.length; i++) {
         //Create containers for data info
-        let div = document.createElement('div');
+        const div = document.createElement('div');
         div.classList.add('dataContainer');
         document.getElementById('container').appendChild(div);
 
         //Append data info to containers
-        let name = document.createElement('p');
-        let population = document.createElement('p');
+        const name = document.createElement('h3');
+        const population = document.createElement('p');
+        const id = document.createElement('p');
 
         name.textContent = data[i].name;
-        population.textContent = data[i].population;
+        population.textContent = "Population: " + data[i].population;
+        id.textContent = "Id: " + data[i].id;
 
         document.querySelectorAll('.dataContainer')[i].appendChild(name);
         document.querySelectorAll('.dataContainer')[i].appendChild(population);
+        document.querySelectorAll('.dataContainer')[i].appendChild(id);
 
       }
 
     })
 })
 
-const form = document.querySelector('#form');
+//POST function
+const putForm = document.querySelector('#postForm');
 
-form.addEventListener('submit', (e) => {
+putForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   //Get value of first field
-  const getNameValue = document.querySelector('#cityName').value;
+  const getNameValueToPost = document.querySelector('#cityNamePOST').value;
   //Get value from second inputfield and convert it to a number
-  let getPopulationValue = document.querySelector('#population').value * 1;
+  const getPopulationValue = document.querySelector('#populationPOST').value * 1;
 
-
-  console.log(getPopulationValue);
 
   //Put values into an object
-  const data = {
-    name: getNameValue,
+  const postData = {
+    name: getNameValueToPost,
     population: getPopulationValue
   }
 
@@ -52,8 +54,38 @@ form.addEventListener('submit', (e) => {
       'Content-Type': 'application/json'
     },
     //Convert our JavaScript value into JSON value before posting
-    body: JSON.stringify(data)
+    body: JSON.stringify(postData)
   })
     .then(res => res.json())
     .then(data => console.log(data))
+})
+
+//PUT function
+const postForm = document.querySelector('#putForm');
+
+postForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const getNameValueToPut = document.querySelector('#cityNamePUT').value;
+
+  const getPopulationValueToPut = document.querySelector('#populationPUT').value * 1;
+
+  const getCityIdToPut = document.querySelector('#cityIdPUT').value;
+
+  console.log(getCityIdToPut);
+
+  const putData = {
+    id: getCityIdToPut,
+    name: getNameValueToPut,
+    population: getPopulationValueToPut
+  }
+
+  fetch('https://avancera.app/cities/' + `${getCityIdToPut}`, {
+    body: JSON.stringify(putData),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'PUT'
+  })
+    .then(res => console.log(res));
 })
